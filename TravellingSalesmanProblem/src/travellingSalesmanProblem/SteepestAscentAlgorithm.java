a63aec920f29d03b6982723a6d33bb1e30d53c1b
JonRaw89 <jprawsthorne@gmail.com>
2023-05-30 20:52:08 +0100

    steepest ascent algo

--- TravellingSalesmanProblem/src/travellingSalesmanProblem/SteepestAscentAlgorithm.java
+++ TravellingSalesmanProblem/src/travellingSalesmanProblem/SteepestAscentAlgorithm.java
@@ -2,50 +2,59 @@ package travellingSalesmanProblem;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Random;
 
-public class BasicHillClimbingAlgorithm {
+public class SteepestAscentAlgorithm {
 	
-	private final int MAX_ITERATIONS = 25;
+	private final int MAX_ITERATIONS = 100;
 	private int numCities;
-	private Random rand = new Random();
 	
 	private int[][] tspDistances;
+	private Random rand = new Random();
 
-	public BasicHillClimbingAlgorithm(int[][] tspDistances) {
+	public SteepestAscentAlgorithm(int[][] tspDistances) {
 		
 		this.tspDistances = tspDistances;
 		this.numCities = tspDistances.length;
 		
 	}
 	
+	
 	public int solve(int[] solution) {
 		
-		int i = 0;
-		while(i < MAX_ITERATIONS) {
-			
-			// random initial solution
-			int[] newSolution = solution.clone();	
-			int city1 = rand.nextInt(numCities-1);
-			int tempSwap = newSolution[city1];
-			newSolution[city1] = newSolution[city1+1];
-			newSolution[city1+1] = tempSwap;
-			
-			if (calculateTotalDistance(newSolution) < calculateTotalDistance(solution)) {
-				solution = newSolution;
-				//resets count to 0
-				i = 0;
-			}
-			else {
-				i++;
-			}
-		}
-		System.out.println("Final Solution: " + Arrays.toString(solution));
-		System.out.println("Final Solution Distance: " + calculateTotalDistance(solution) + "\n");
-		
-		return calculateTotalDistance(solution);
-		
+	    int i = 0;
+	    
+	    while(i < MAX_ITERATIONS) {
+	    	
+	        int[] newSolution = null;	
+	        int bestDistance = calculateTotalDistance(solution);
+
+	        for (int city = 0; city < numCities - 1; city++) {
+	            int[] tempSolution = solution.clone();	
+	            int tempSwap = tempSolution[city];
+	            
+	            tempSolution[city] = tempSolution[city+1];
+	            tempSolution[city+1] = tempSwap;
+	            
+	            int tempDistance = calculateTotalDistance(tempSolution);
+
+	            if (tempDistance < bestDistance) {
+	                bestDistance = tempDistance;
+	                newSolution = tempSolution;
+	            }
+	        }
+	        if (newSolution != null) {
+	            solution = newSolution;
+	            i = 0;
+	            
+	        } else {
+	            i++;
+	        }
+	    }
+	    System.out.println("Steepest Final Solution: " + Arrays.toString(solution));
+	    System.out.println("Steepest Final Solution Distance: " + calculateTotalDistance(solution) + "\n");
+
+	    return calculateTotalDistance(solution);
 	}
 	
 	public int[] greedy() {
@@ -67,7 +76,7 @@ public class BasicHillClimbingAlgorithm {
 		return solution;
 		
 	}	
-
+	
 	public int findNextCity(int city, ArrayList<Integer> visitedCities) {
 		
 		int nearest = -1;
@@ -93,18 +102,6 @@ public class BasicHillClimbingAlgorithm {
 		
 	}
 	
-	public int[] random(){
-		ArrayList<Integer> randomSolution = new ArrayList<>();
-		
-		for (int i = 0; i < numCities; i++) {
-			randomSolution.add(i);
-			}
-		Collections.shuffle(randomSolution);
-		
-		return randomSolution.stream().mapToInt(i -> i).toArray();
-	}
-	
-	
 	public int calculateTotalDistance(int[] solution) {
 		
 		int totalDistance = 0;
@@ -119,7 +116,5 @@ public class BasicHillClimbingAlgorithm {
 		return totalDistance;
 	}
 	
-
-
 	
 }
--- TravellingSalesmanProblem/src/travellingSalesmanProblem/TravellingSalesmanProblem.java
+++ TravellingSalesmanProblem/src/travellingSalesmanProblem/TravellingSalesmanProblem.java
@@ -2,12 +2,16 @@ package travellingSalesmanProblem;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TravellingSalesmanProblem {	 
 
-	public static int[][] readSalesmanData() throws IOException {
+	public static int[][] readSalesmanFileData() throws IOException {
 		
 	    ArrayList<int[]> list = new ArrayList<>();
 	    File fileObj = new File("C:/Users/jpraw/git/ts_problem/TravellingSalesmanProblem/src/travellingSalesmanProblem/data.txt");
@@ -29,9 +33,44 @@ public class TravellingSalesmanProblem {
 	    return tspDistances;
 	}
 	
-	 public static void main(String[] args) throws IOException {
+	public static int[][] readSalesmanCsvData() throws IOException {
+		
+        List<double[]> coordinates = new ArrayList<>();
+
+        Path path = Paths.get("C:/Users/jpraw/git/ts_problem/TravellingSalesmanProblem/src/travellingSalesmanProblem/TSP_matrix.csv");
+        List<String> lines = Files.readAllLines(path);
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            double x = Double.parseDouble(parts[0]);
+            double y = Double.parseDouble(parts[1]);
+            coordinates.add(new double[]{x, y});
+        }
+
+        int size = coordinates.size();        
+        int[][] tspDistances = new int[size][size];
+
+        // work out euclidean distances between coords.
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j) {
+                    tspDistances[i][j] = 0;
+                } else {
+                    double[] ci = coordinates.get(i);
+                    double[] cj = coordinates.get(j);
+                    double distance = Math.sqrt(Math.pow(ci[0] - cj[0], 2) + Math.pow(ci[1] - cj[1], 2));
+                    tspDistances[i][j] = (int) Math.round(distance);
+                }
+            }
+        }
+
+        return tspDistances;
+    }
+	    
+	
+	
+	public static void main(String[] args) throws IOException {
 		   
-		 BasicHillClimbingAlgorithm bhc = new BasicHillClimbingAlgorithm(readSalesmanData());
+		 BasicHillClimbingAlgorithm bhc = new BasicHillClimbingAlgorithm(readSalesmanFileData());
 		 
 		 int averageRandom = 0;
 		 int averageGreedy = 0;
@@ -39,16 +78,23 @@ public class TravellingSalesmanProblem {
 		 int[] random = bhc.random();
 		 int[] greedy = bhc.greedy();
 		 
-		 for (int i = 0; i < 10; i++) {
-			 
-			 // Random Solution
-			 averageRandom += bhc.solve(random);
-
-			 // Greedy Solution
-			 averageGreedy += bhc.solve(greedy);
-		 }
+//		 for (int i = 0; i < 10; i++) {
+//			 
+//			 // Random Solution
+//			 averageRandom += bhc.solve(random);
+//
+//			 // Greedy Solution
+//			 averageGreedy += bhc.solve(greedy);
+//		 }
+//		 
+//		 System.out.println("Average Random distance: " + averageRandom/10);
+//		 System.out.println("Average Greedy distance: " + averageGreedy/10);
 		 
-		 System.out.println("Average Random distance: " + averageRandom/10);
-		 System.out.println("Average Greedy distance: " + averageGreedy/10);
+		 SteepestAscentAlgorithm steep = new SteepestAscentAlgorithm(readSalesmanCsvData());
+		 int[] greedySteep = steep.greedy();
+		 
+		 
+		 steep.solve(greedySteep);
+		 
 	 }
 }
